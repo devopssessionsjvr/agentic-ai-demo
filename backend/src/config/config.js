fccef2a751d8c58222f64d699438b96ef179e2ff
Jayavardhan <jvr.vardhan@gmail.com>
2026-04-08 17:22:35 -0400

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,1 +1,13 @@
 
+// Dummy config for demo
+const getEnv = (key) => process.env[key] || "demo";
+
+export const config = {
+  mongoUrl: getEnv("MONGO_URL"),
+  accessTokenSecret: getEnv("ACCESS_TOKEN_SECRET"),
+  rapidApiKey: getEnv("RAPID_API_KEY"),
+  googleAIKey: getEnv("GOOGLE_GENERATIVE_AI_KEY"),
+  stripeSecretKey: getEnv("STRIPE_SECRET_KEY"),
+  emailUser: getEnv("EMAIL_USER"),
+  emailPassword: getEnv("EMAIL_PASSWORD"),
+};
